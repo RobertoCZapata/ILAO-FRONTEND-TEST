e49ddc54b13faf910f96f23813406a26b1a4cd5e
RobertoZapata <Roberto.Carlos.Zapata.Reyes.-ND@disney.com>
2024-11-14 09:31:55 -0500

    filter and responsive design

--- app/static/js/general.js
+++ app/static/js/general.js
@@ -1,4 +1,11 @@
 import { Common } from './common/index.js';
+import { productFilter } from './utils/filters.js';
 
-Common();
+Common(() => {
+  document.addEventListener('alpine:init', () => {
+    Alpine.data('productFilter', productFilter);
+  });
 
+  // Llama a productFilter para inicializar
+  productFilter();
+});
